f7153b9f0b63b2e6abe754f51c483e32306bc3bd
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-17 14:02:19 -0700

    server is updated

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express'),
+    bodyparser = require('body-parser'),
+    methodoverride = require('method-override'),
+    app = express(),
+    port = process.env.PORT || 5000;
+
+//========================MIDDLEWARE========================\\
+
+// Dependencies
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(methodoverride('_method'));
+
+// Mongoose Connection
+require('./data/database.js');
+
+// Routers
+
+// Access Port
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
