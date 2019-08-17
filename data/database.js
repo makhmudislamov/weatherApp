e39fbb11e1859a1ed0efc647f3bbe4b6c1a6fac0
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-17 14:00:24 -0700

    connected database

---
+++ data/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'),
+    assert = require('assert');
+
+const url = process.env.MONGODB_URI || "mongodb://localhost/weathermood-db"; 
+
+mongoose.connect(url, { useNewUrlParser: true },
+    function (err, _) {
+        assert.equal(null, err)
+        console.log('DB connection successful.')
+    },
+    { useFindAndModify: false }
+);
+
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
